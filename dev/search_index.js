19e2cbde29059e072bfbff1ffa75cf392e866e08
Documenter.jl <documenter@juliadocs.github.io>
2023-09-11 12:20:22 +0000

    build based on 96aa254

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#NmodController.jl","page":"NmodController.jl","title":"NmodController.jl","text":"","category":"section"},{"location":"","page":"NmodController.jl","title":"NmodController.jl","text":"Documentation for NmodController.jl","category":"page"},{"location":"","page":"NmodController.jl","title":"NmodController.jl","text":"Writing some tests","category":"page"}]
+}
